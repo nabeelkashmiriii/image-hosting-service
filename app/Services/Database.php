7f47e5f6779c19f0e4300d8238adcfc355b46418
Nabeel Tariq <nabeeltariq261@gmail.com>
2021-12-14 12:38:06 +0500

    db config

--- app/Services/Database.php
+++ app/Services/Database.php
@@ -13,9 +13,9 @@ class  Database
     // constructer For Database Connection
     public function __construct($collection)
     {
-        $connection_string = 'mongodb://kashmiriii:kashmiriii@image-hosting-service-shard-00-00.hx6kr.mongodb.net:27017,image-hosting-service-shard-00-01.hx6kr.mongodb.net:27017,image-hosting-service-shard-00-02.hx6kr.mongodb.net:27017/user?replicaSet=atlas-v8ytco-shard-0&ssl=true&authSource=admin';
+        $connection_string = 'mongodb+srv://kashmiriii:kashmiriii@image-hosting-service.hx6kr.mongodb.net/imageHostingService?retryWrites=true&w=majority';
         $conn= new DB($connection_string);
-        $this->connection  = $conn->imageHostingService;
+        $this->connection  = $conn->imageHostingService->$collection;
     }
 
     // Get Conection

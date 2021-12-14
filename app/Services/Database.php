bf293eb580a69ba2016bdb994debac2a78b31a51
Nabeel Tariq <nabeeltariq261@gmail.com>
2021-12-14 12:33:42 +0500

    More Better

--- app/Services/Database.php
+++ app/Services/Database.php
@@ -13,9 +13,9 @@ class  Database
     // constructer For Database Connection
     public function __construct($collection)
     {
-        $connection_string = 'mongodb://kashmiriii:kashmiriii@image-hosting-service-shard-00-00.hx6kr.mongodb.net:27017,image-hosting-service-shard-00-01.hx6kr.mongodb.net:27017,image-hosting-service-shard-00-02.hx6kr.mongodb.net:27017/test?replicaSet=atlas-v8ytco-shard-0&ssl=true&authSource=admin';
+        $connection_string = 'mongodb://kashmiriii:kashmiriii@image-hosting-service-shard-00-00.hx6kr.mongodb.net:27017,image-hosting-service-shard-00-01.hx6kr.mongodb.net:27017,image-hosting-service-shard-00-02.hx6kr.mongodb.net:27017/user?replicaSet=atlas-v8ytco-shard-0&ssl=true&authSource=admin';
         $conn= new DB($connection_string);
-        $this->connection  = $conn->imageHostingService->$collection;
+        $this->connection  = $conn->imageHostingService;
     }
 
     // Get Conection

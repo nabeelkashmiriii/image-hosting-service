2e9f590ec1e44ce673ca1c04e0bac03756ab6a40
Nabeel Tariq <nabeeltariq261@gmail.com>
2021-12-14 12:14:44 +0500

    database changed

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'mongodb'),
 
     /*
     |--------------------------------------------------------------------------
@@ -34,6 +34,10 @@ return [
     */
 
     'connections' => [
+        'mongodb' => [
+            'driver' => 'mongodb',
+            'dsn' => env('DB_URI', 'mongodb+srv://username:password@<atlas-cluster-uri>/myappdb?retryWrites=true&w=majority'),
+            'database' => 'myappdb',
 
         'sqlite' => [
             'driver' => 'sqlite',
